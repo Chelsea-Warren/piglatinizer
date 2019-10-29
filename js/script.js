2cca15b5eeaebbc6ed900973903cbb23d9809be4
Chelsea-Warren <warrenchelsea546@gmail.com>
2019-10-29 22:16:07 +0000

    hi

--- js/script.js
+++ js/script.js
@@ -1,9 +1,16 @@
 $( "document" ).ready(function() { //this helps your jQuery to work. Write all of your JS code in this function.
+$("#button").click(function(){
+    let input=$(".input").val();
+    alert(input);
 
 
 
-  
-  
-  
-  
+
+});
+
+
+
+
+
+
 });
